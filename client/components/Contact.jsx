cf40f781722819b1a72df1be3d678a4c30e5fb13
harrison-symes <symeshjb@gmail.com>
2017-08-07 09:54:47 +1200

    failed attempt to use the react-google-maps module

--- client/components/Contact.jsx
+++ client/components/Contact.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import {connect} from 'react-redux'
+import Map from './Map'
 
 const Contact = ({dispatch}) => (
   <div className="Contact container tile is-anscestor has-text-centered">
     <div className="tile is-parent is-vertical is-6 content box">
       <h1 className="title is-fluid">Contact</h1>
-      <div className="Contact-Details media">
-        <p className="">For More information on training with me at Les Mills Dunedin, please use the following contact addresses:</p>
-        <ul className="">
+      <div className="Contact-Details">
+        <p className="media">For More information on training with me at Les Mills Dunedin, please use the following contact addresses:</p>
+        <ul className="container">
           <li>Mobile: 027 445 3965</li>
           <li>Email: maxsymes@hotmail.com</li>
           <li>Address: Les Mills,
@@ -15,6 +16,9 @@ const Contact = ({dispatch}) => (
             Dunedin, 9016</li>
           </ul>
       </div>
+    </div>
+    <div className="tile is-parent is-vertical is-6 box">
+      <Map />
     </div>
   </div>
 
---
+++ client/components/Map.jsx
@@ -0,0 +1,61 @@
+import React from 'react'
+import { withGoogleMap, GoogleMap, Marker } from "react-google-maps";
+const googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.27&libraries=places,geometry&key=AIzaSyBXthm1gu3zhhwKzWA0EPSW_TI-XY9MEIU"
+
+//AIzaSyBXthm1gu3zhhwKzWA0EPSW_TI-XY9MEIU
+const google = window.google;
+
+const GettingStartedGoogleMap = withGoogleMap(props => (
+  <GoogleMap
+    ref={props.onMapLoad}
+    defaultZoom={3}
+    defaultCenter={{ lat: -25.363882, lng: 131.044922 }}
+    onClick={props.onMapClick}
+    googleMapURL={googleMapURL}
+  >
+    {props.markers.map((marker, index) => (
+      <Marker
+        {...marker}
+        onRightClick={() => props.onMarkerRightClick(index)}
+      />
+    ))}
+    {console.log(props)}
+  </GoogleMap>
+))
+
+export default function Map(props) {
+  return (
+    <GettingStartedGoogleMap
+      containerElement={
+        <div style={{ height: `100%` }} />
+      }
+      mapElement={
+        <div style={{ height: `100%` }} />
+      }
+      onMapLoad={_.noop}
+      onMapClick={_.noop}
+      markers={markers}
+      onMarkerRightClick={_.noop}
+    />
+  )
+}
+
+
+
+
+//
+// export default class Map extends React.Component {
+//   constructor(props) {
+//     super(props)
+//     this.state = {
+//       style: {
+//         width: '100%',
+//         height: '100%',
+//         backgroundColor: 'grey'
+//       }
+//     }
+//   }
+//   render() {
+//
+//   }
+// }
